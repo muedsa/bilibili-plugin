cb9756ac53ab83e854dccfde834dd714e5318727
MUEDSA <7676275+muedsa@users.noreply.github.com>
2024-11-28 17:47:12 +0800

    feat: 暂不提供目录功能

--- app/src/main/java/com/muedsa/tvbox/bilibili/service/MediaCatalogService.kt
+++ app/src/main/java/com/muedsa/tvbox/bilibili/service/MediaCatalogService.kt
@@ -5,19 +5,22 @@ import com.muedsa.tvbox.api.data.MediaCatalogConfig
 import com.muedsa.tvbox.api.data.MediaCatalogOption
 import com.muedsa.tvbox.api.data.PagingResult
 import com.muedsa.tvbox.api.service.IMediaCatalogService
+import com.muedsa.tvbox.bilibili.BilibiliConst
 
 class MediaCatalogService(
 ) : IMediaCatalogService {
 
-    override suspend fun getConfig(): MediaCatalogConfig {
-        TODO()
-    }
+    override suspend fun getConfig(): MediaCatalogConfig =
+        MediaCatalogConfig(
+            initKey = "1",
+            pageSize = 1,
+            cardWidth = BilibiliConst.AV_CARD_WIDTH,
+            cardHeight = BilibiliConst.AV_CARD_HEIGHT,
+        )
 
     override suspend fun catalog(
         options: List<MediaCatalogOption>,
         loadKey: String,
         loadSize: Int
-    ): PagingResult<MediaCard> {
-        TODO()
-    }
+    ): PagingResult<MediaCard> = PagingResult()
 }
