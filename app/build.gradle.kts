b246c9b317574f5daa85a38814993fee8724d9c4
MUEDSA <7676275+muedsa@users.noreply.github.com>
2026-06-11 18:19:54 +0800

    release: 0.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.muedsa.tvbox.bilibili"
         minSdk = 24
         targetSdk = 36
-        versionCode = 24
-        versionName = "0.4.0"
+        versionCode = 25
+        versionName = "0.4.1"
     }
 
     signingConfigs {
